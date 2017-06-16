ad0ed32a3ec231e9e92faaa0efea3e201a64f968
genarvaez <ge_naravez@hotmail.com>
2017-06-15 23:21:55 -0400

    maqueta inicial de index2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 var usuario = document.getElementById("usuario");
 var password = document.getElementById("password");
 var button = document.getElementById("button");
+var invalido = Array.from(document.getElementsByClassName("invalido"));
 
 
 
-button.addEventListener("click", function(){
-	if(usuario.value == "" || password.value == ""){
-		alert("no")
-	}
+button.addEventListener("click", function(event){
+	if(usuario.value == "" || password.value == "" || usuario.value.length < 6 || password.value.length < 6){
+		event.preventDefault()
+		invalido.forEach(function(e){
+			e.style.display = "block";
+		})
+	}	
 })
